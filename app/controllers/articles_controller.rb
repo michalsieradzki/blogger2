e72a171266f43d05698a386ac7a90df4e13c342f
michalsieradzki <michal.sieradzki@mail.com>
2022-08-01 15:44:22 +0200

    formularze

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,46 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(strong_article_params)
+    @article.save
+    
+    flash.notice = "Artykuł #{@article.title} został utworzony"
+    
+    redirect_to article_path(@article)
+  end
+
+  def delete
+    @article = Article.find(params[:id])
+    @article.delete
+
+    flash.notice = "Artykuł #{@article.title} został usunięty"
+
+    redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(strong_article_params)
+
+    flash.notice = "Artykuł #{@article.title} został zaktualizowany"
+
+    redirect_to article_path(@article)
+  end
+
+
+
+  def strong_article_params
+    params.require(:article).permit(:title, :body)
+  end
+
 end
